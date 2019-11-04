eca573a05ac39ddd6fbc9cfdeb3a939159966b49
Mirco <mirco@x220>
2019-11-04 22:04:24 +0100

    added about page

---
+++ src/app/pages/about/about.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from './components/about/about.component';
+import { RouterModule } from '@angular/router';
+import { aboutRoutes } from './routes';
+
+
+
+@NgModule({
+  declarations: [AboutComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(aboutRoutes)
+  ]
+})
+export class AboutModule { }
---
+++ src/app/pages/about/routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+
+export const aboutRoutes: Routes = [
+    {
+        path: 'about',
+        component: AboutComponent
+    }
+]
