72b7934e1361416f385430ee97f8dee93967926b
dkf10 <mirco.df@live.it>
2019-11-05 11:49:41 +0100

    pokemon detail - fix binding and temporary mocked data

--- src/app/pages/pokemon/pokemon/pokemon.component.ts
+++ src/app/pages/pokemon/pokemon/pokemon.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const spriteBasePath = environment.SPRITE_BASE_PATH;
 
 @Component({
   selector: 'app-pokemon',
@@ -6,11 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon.component.scss']
 })
 export class PokemonComponent implements OnInit {
-  public pokemonId: string = "1";
+  public spritePic: string;
+  public flavorText: string;
+  
+  @Input("pokemonName") pokemonName: string;
+  @Input("pokemonId") pokemonId: string;
 
   constructor() { }
 
   ngOnInit() {
+    //TODO: REMOVE, MOCKED DATA
+    this.pokemonId = "1"; 
+    this.pokemonName = "Bulbasaur"
+    this.flavorText = "Bulbasaur can be seen napping in bright sunlight.\nThere is a seed on its back. By soaking up the sun’s rays,\nthe seed grows progressively larger."
+    
+    this.spritePic = spriteBasePath + this.pokemonId + ".png";
   }
 
 }
