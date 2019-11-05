fbf11e974f72e9901acfad8d0aba780f060c7f84
dkf10 <mirco.df@live.it>
2019-11-05 15:53:05 +0100

    pokemon detail - completed pokemon interface

--- src/app/pages/pokemon/components/interfaces/pokemon.interface.ts
+++ src/app/pages/pokemon/components/interfaces/pokemon.interface.ts
@@ -1,3 +1,31 @@
-export interface PokemonInterface {
-    
+export interface PokemonInterface { 
+    abilities: PAbility[],
+    base_experience: string,
+    height: string,
+    moves: PMove[],
+    name: string,
+    stats: PStat[],
+    types: PType[],
+    weight: string
+}
+
+export interface PAbility {
+    name: string,
+    url: string
+}
+
+export interface PMove {
+    name: string,
+    url: string
+}
+
+export interface PStat {
+    base_stat: string,
+    name: string,
+    url: string
+}
+
+export interface PType {
+    name: string,
+    url: string
 }
