ebede89aa4f09591b5d28a5610defc4a2dd63143
dkf10 <mirco.df@live.it>
2019-11-05 15:02:41 +0100

    pokedex - improved next previous page buttons

--- src/app/pages/pokedex/components/pokedex/pokedex.component.ts
+++ src/app/pages/pokedex/components/pokedex/pokedex.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpParams } from '@angular/common/http';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { faAngleDoubleLeft, faAngleDoubleRight, IconDefinition } from '@fortawesome/free-solid-svg-icons'
+
 import { PokedexService } from '../../services/pokedex.service';
 import { Pokemon } from '../../interfaces/pokedex.interface';
 import { AlertService } from 'src/app/shared/services/alert/alert.service';
@@ -16,7 +18,17 @@ export class PokedexComponent implements OnInit {
   public pokemon: Pokemon[];
   public countedPokemon: number;
 
-  constructor(private pokedexService: PokedexService, private alertService: AlertService, private spinner: NgxSpinnerService) { }
+  public prevPgIcon: IconDefinition;
+  public nextPgIcon: IconDefinition;
+
+  constructor(
+    private pokedexService: PokedexService, 
+    private alertService: AlertService, 
+    private spinner: NgxSpinnerService
+  ) {
+    this.prevPgIcon = faAngleDoubleLeft;
+    this.nextPgIcon = faAngleDoubleRight;
+  }
 
   ngOnInit() {
     this.loadPokedex();
--- src/app/pages/pokedex/pokedex.module.ts
+++ src/app/pages/pokedex/pokedex.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PokedexComponent } from './components/pokedex/pokedex.component';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
+import { PokedexComponent } from './components/pokedex/pokedex.component';
 import { pokedexRoutes } from './routes';
 import { PokecardComponent } from './components/pokecard/pokecard.component';
 
@@ -11,7 +13,8 @@ import { PokecardComponent } from './components/pokecard/pokecard.component';
   declarations: [PokedexComponent, PokecardComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(pokedexRoutes)
+    RouterModule.forChild(pokedexRoutes),
+    FontAwesomeModule
   ]
 })
 export class PokedexModule { }
