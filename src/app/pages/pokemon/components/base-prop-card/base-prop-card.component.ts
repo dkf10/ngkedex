4b073322b4e3a782836cc242cc9b1ed97c57da1d
dkf10 <mirco.df@live.it>
2019-11-06 18:23:31 +0100

    pokemon detail - completed base properties cards

--- src/app/pages/pokemon/components/base-prop-card/base-prop-card.component.ts
+++ src/app/pages/pokemon/components/base-prop-card/base-prop-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ngkx-base-prop-card',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./base-prop-card.component.scss']
 })
 export class BasePropCardComponent implements OnInit {
+
+  @Input("prop") basePropTitle: string;
+  @Input("value") basePropValue: string;
 
   constructor() { }
 
