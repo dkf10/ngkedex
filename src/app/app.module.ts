9d71d74b8607ecad1177fdc5568c026bf2524d34
dkf10 <mirco.df@live.it>
2019-11-07 12:07:17 +0100

    removed news page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './shared/components/components.module';
 import { PokedexModule } from './pages/pokedex/pokedex.module';
-import { NewsModule } from './pages/news/news.module';
 import { AboutModule } from './pages/about/about.module';
 import { ServicesModule } from './shared/services/services.module';
 import { PokemonModule } from './pages/pokemon/pokemon.module';
@@ -19,7 +18,6 @@ import { PokemonModule } from './pages/pokemon/pokemon.module';
     AppRoutingModule,
     ComponentsModule,
     PokedexModule,
-    NewsModule,
     AboutModule,
     ServicesModule,
     PokemonModule
--- src/app/pages/news/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { NewsComponent } from './components/news/news.component';
-
-export const newsRoutes: Routes = [
-    {
-        path: 'news',
-        component: NewsComponent
-    }
-]
